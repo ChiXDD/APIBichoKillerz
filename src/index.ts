a47c43ff62ea17fb79229b527fb3c4d2e3eb1781
ChiXDD <danychi2003@gmail.com>
2024-11-22 20:59:23 -0600

    Archivo principal

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors';
+import routes from './routes/routes'
+
+const app = express()
+
+app.use(cors({
+  origin: 'http://localhost:5173', // Ajusta según donde esté corriendo tu app
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.use(express.json());
+app.use('/', routes);
+
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+})
