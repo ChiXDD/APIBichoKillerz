d118003e753cc26eb8d300b20b50d80bcbe00f85
ChiXDD <danychi2003@gmail.com>
2024-11-22 20:58:45 -0600

    Conexión a la base de datos

---
+++ src/BD/connection.ts
@@ -0,0 +1,16 @@
+import mysql from 'mysql2/promise'
+
+const accesData: mysql.ConnectionOptions = {
+  user: 'root',
+  host: 'localhost',
+  database: 'BichoKillerz',
+  password: '10111011',
+  port: 3306,
+  connectionLimit: 10,
+  multipleStatements: true,
+  queueLimit: 0,
+}
+
+const connection = mysql.createPool(accesData)
+
+export default connection
