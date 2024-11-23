ee9b80521b74b120825fe83714ad70ba806c6eff
ChiXDD <danychi2003@gmail.com>
2024-11-23 02:38:55 -0600

    Corrección de formatos

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,11 @@
-import { Router } from 'express';
-import productosRoutes from './productosRoutes';
-import serviciosRoutes from './serviciosRoutes';
+import { Router } from 'express'
+import productosRoutes from './productosRoutes'
+import serviciosRoutes from './serviciosRoutes'
 
-const router = Router();
+const router = Router()
 
 // Prefijo /api/productos para todas las rutas de productos
-router.use('/productos', productosRoutes);
-router.use('/servicios', serviciosRoutes);
+router.use('/productos', productosRoutes)
+router.use('/servicios', serviciosRoutes)
 
-export default router;
+export default router
